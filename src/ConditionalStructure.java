d914f466fc8d1875c259c7702cd721771398853c
Paulo07Reis <pmarcena21@gmail.com>
2025-07-03 20:34:05 -0300

    feat: If and Else structure

---
+++ src/ConditionalStructure.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ConditionalStructure {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Insert a number: ");
+        int number = sc.nextInt();
+        sc.close();
+
+        if(number < 0){
+            System.out.println("The number is less than 0");
+        } else if (number <= 10) {
+            System.out.println("The number is less than 10 or is 10");
+        } else {
+            System.out.println("The number is greater than 10!");
+        }
+    }
+}
