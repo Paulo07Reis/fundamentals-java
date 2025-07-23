9e214c83bd98b89aee2111e658db33566af4f26b
Paulo07Reis <pmarcena21@gmail.com>
2025-07-23 17:36:26 -0300

    feat: Application class

---
+++ src/poo/exercise/exercise12/Application.java
@@ -0,0 +1,67 @@
+package poo.exercise.exercise12;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Application {
+    public static void main(String[] args) throws ParseException {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+        System.out.println("Enter client data: ");
+        System.out.println();
+
+        System.out.print("Name: ");
+        String clientName = sc.nextLine();
+
+        System.out.print("Email: ");
+        String clientEmail = sc.nextLine();
+
+        System.out.print("Birth date (DD/MM/YYYY): ");
+        Date clientBirthDate = sdf.parse(sc.nextLine());
+
+        Client client = new Client(clientName, clientEmail, clientBirthDate);
+
+        System.out.println();
+
+        System.out.println("Enter order data:");
+
+        System.out.print("Status: ");
+        OrderStatus orderStatus = OrderStatus.valueOf(sc.nextLine());
+
+        Order order = new Order(client, new Date(), orderStatus);
+
+        System.out.println();
+        System.out.print("How many items to this order: ");
+        int itemsQuantity = sc.nextInt();
+        sc.nextLine();
+
+        System.out.println();
+        for (int i = 1; i <= itemsQuantity ; i++) {
+            System.out.println("Enter #" + i + " item data: ");
+            System.out.println();
+
+            System.out.print("Product name: ");
+            String productName = sc.nextLine();
+
+            System.out.print("Product price: ");
+            double productPrice = sc.nextDouble();
+
+            System.out.print("Quantity: ");
+            int productQuantity = sc.nextInt();
+            sc.nextLine();
+
+            Product product = new Product(productName, productPrice);
+            OrderItem orderItem = new OrderItem(productQuantity, product);
+            order.addItem(orderItem);
+            System.out.println();
+        }
+
+        System.out.println(order);
+
+    }
+}
