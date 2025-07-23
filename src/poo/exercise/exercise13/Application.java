cc972b6e08d00f2cbad850d9d802fd3242a0f574
Paulo07Reis <pmarcena21@gmail.com>
2025-07-23 19:21:12 -0300

    feat: Working with heritage

---
+++ src/poo/exercise/exercise13/Application.java
@@ -0,0 +1,11 @@
+package poo.exercise.exercise13;
+
+import poo.exercise.exercise13.domain.BusinessAccount;
+
+public class Application {
+    public static void main(String[] args) {
+        BusinessAccount businessAccount = new BusinessAccount();
+
+        businessAccount.getBalance();
+    }
+}
