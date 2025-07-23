62da88b72d9cb64b13e3c0f545b1186c13085fa2
Paulo07Reis <pmarcena21@gmail.com>
2025-07-23 17:36:47 -0300

    feat: OrderItem class

---
+++ src/poo/exercise/exercise12/OrderItem.java
@@ -0,0 +1,38 @@
+package poo.exercise.exercise12;
+
+public class OrderItem {
+    private Integer quantity;
+    private Product product;
+
+    public OrderItem() {}
+
+    public OrderItem(Integer quantity, Product product) {
+        this.quantity = quantity;
+        this.product = product;
+    }
+
+    public Double subTotal(){
+        return quantity * product.getPrice();
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    @Override
+    public String toString() {
+        return product + ", Quantity: " + quantity + ", Subtotal: $" + subTotal();
+    }
+}
