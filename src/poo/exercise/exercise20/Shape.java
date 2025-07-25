3e91c2d1d17a05e0208cfe3acee9fd41be840a7f
Paulo07Reis <pmarcena21@gmail.com>
2025-07-25 18:03:45 -0300

    feat: Shape class

---
+++ src/poo/exercise/exercise20/Shape.java
@@ -0,0 +1,21 @@
+package poo.exercise.exercise20;
+
+public abstract class Shape {
+    private Color color;
+
+    public Shape() {}
+
+    public Shape(Color color) {
+        this.color = color;
+    }
+
+    public abstract double area();
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+}
