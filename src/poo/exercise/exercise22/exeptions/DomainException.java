c42d17f3db3f077c5f0720e2d3c5acca415e97d3
Paulo07Reis <pmarcena21@gmail.com>
2025-07-26 16:38:59 -0300

    feat: Building exception class

---
+++ src/poo/exercise/exercise22/exeptions/DomainException.java
@@ -0,0 +1,7 @@
+package poo.exercise.exercise22.exeptions;
+
+public class DomainException extends RuntimeException {
+    public DomainException(String message) {
+        super(message);
+    }
+}
