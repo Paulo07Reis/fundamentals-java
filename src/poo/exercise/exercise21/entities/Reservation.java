ded047fba92c18959f9c8ebf76d4d43f07000c7b
Paulo07Reis <pmarcena21@gmail.com>
2025-07-26 12:39:45 -0300

    feat: Building Reservation class

---
+++ src/poo/exercise/exercise21/entities/Reservation.java
@@ -0,0 +1,55 @@
+package poo.exercise.exercise21.entities;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class Reservation {
+    private Integer roomNumber;
+    private Date checkIn;
+    private Date checkOut;
+
+    private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+    public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
+        this.roomNumber = roomNumber;
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
+
+    public long duration(){
+        long diff = checkOut.getTime() - checkIn.getTime();
+        return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+    }
+
+    public void updateDate(Date checkIn, Date checkOut){
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
+
+    public Integer getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(Integer roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public Date getCheckIn() {
+        return checkIn;
+    }
+
+    public Date getCheckOut() {
+        return checkOut;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Room: ").append(roomNumber);
+        sb.append(", check-in: ").append(sdf.format(checkIn));
+        sb.append(", check-out: ").append(sdf.format(checkOut));
+        sb.append(", ").append(duration()).append(" nights");
+        return sb.toString();
+    }
+}
