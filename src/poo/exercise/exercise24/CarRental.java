6ec4b4f61815b8d95a4212a9eb8e0400b71e0d32
Paulo07Reis <pmarcena21@gmail.com>
2025-07-28 19:28:16 -0300

    feat: Car Rental class to exercise 24

---
+++ src/poo/exercise/exercise24/CarRental.java
@@ -0,0 +1,48 @@
+package poo.exercise.exercise24;
+
+import java.time.LocalDateTime;
+
+public class CarRental {
+    private Vehicle vehicle;
+    private LocalDateTime start;
+    private LocalDateTime finish;
+    private Invoice invoice;
+
+    public CarRental( Vehicle vehicle, LocalDateTime start, LocalDateTime finish) {
+        this.vehicle = vehicle;
+        this.start = start;
+        this.finish = finish;
+    }
+
+    public LocalDateTime getStart() {
+        return start;
+    }
+
+    public void setStart(LocalDateTime start) {
+        this.start = start;
+    }
+
+    public LocalDateTime getFinish() {
+        return finish;
+    }
+
+    public void setFinish(LocalDateTime finish) {
+        this.finish = finish;
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+    public void setVehicle(Vehicle vehicle) {
+        this.vehicle = vehicle;
+    }
+
+    public Invoice getInvoice() {
+        return invoice;
+    }
+
+    public void setInvoice(Invoice invoice) {
+        this.invoice = invoice;
+    }
+}
