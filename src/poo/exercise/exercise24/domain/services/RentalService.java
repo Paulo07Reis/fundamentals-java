1e2912e2edfcbae2056dd087de0870e0748a5186
Paulo07Reis <pmarcena21@gmail.com>
2025-07-28 19:55:35 -0300

    feat: Updating rental service

--- src/poo/exercise/exercise24/domain/services/RentalService.java
+++ src/poo/exercise/exercise24/domain/services/RentalService.java
@@ -8,9 +8,9 @@ import java.time.Duration;
 public class RentalService {
     private Double pricePerDay;
     private Double pricePerHour;
-    private BrazilTaxService taxService;
+    private TaxService taxService;
 
-    public RentalService(Double pricePerDay, Double pricePerHour, BrazilTaxService taxService) {
+    public RentalService(Double pricePerDay, Double pricePerHour, TaxService taxService) {
         this.pricePerDay = pricePerDay;
         this.pricePerHour = pricePerHour;
         this.taxService = taxService;
@@ -48,7 +48,7 @@ public class RentalService {
         this.pricePerHour = pricePerHour;
     }
 
-    public BrazilTaxService getTaxService() {
+    public TaxService getTaxService() {
         return taxService;
     }
 
