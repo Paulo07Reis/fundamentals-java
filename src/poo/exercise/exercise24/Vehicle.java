7c0cdf0e3b1b851b5bcc9538bee4bad5185edc96
Paulo07Reis <pmarcena21@gmail.com>
2025-07-28 19:27:50 -0300

    feat: Vehicle class to exercise 24

---
+++ src/poo/exercise/exercise24/Vehicle.java
@@ -0,0 +1,24 @@
+package poo.exercise.exercise24;
+
+public class Vehicle {
+    private String model;
+
+    public Vehicle(String name) {
+        this.model = name;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Vehicle: ").append(getModel());
+        return sb.toString();
+    }
+}
